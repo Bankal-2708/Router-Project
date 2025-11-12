3fb490d4566df511144ec2788355a059f7c99d57
Bankal Mavi <bankalmavi1@gmail.com>
2025-11-12 15:58:37 +0530

    Project Final

--- 01-routing(mini-project)/src/Components/Github/Github.jsx
+++ 01-routing(mini-project)/src/Components/Github/Github.jsx
@@ -13,14 +13,18 @@ function Github() {
 
   return (
     <div>
-      <div className="text-center m-4 bg-blue-300 text-black text-3xl p-4">
+      <div className=" text-center m-4  bg-cover text-black text-3xl p-4 flex flex-col flex-wrap justify-center items-center md:flex-row md:bg-gray-300 gap-5">
+        <img src={data.avatar_url} alt="Git Picture" width={200 }
+        className="rounded-xl text-center "
+        />
         Github Followers:{data.followers}
+        
       </div>
 
       <div className="flex justify-center items-center m-2">
         <button
         onClick={() => window.open("https://github.com/Bankal-2708", "_blank")}
-        className="  bg-red-700 text-white font-bold border-2 px-5 py-2 rounded-md hover:bg-white hover:text-black hover:border-black "
+        className="  bg-red-700 text-white font-bold border-2 px-7 py-2 rounded-md hover:bg-white hover:text-black hover:border-black "
       >
         GitHub
       </button>
